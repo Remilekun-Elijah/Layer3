368558630980c965ecb4bbde42342b11a28a4077
Remilekun-Elijah <38536025+Remilekun-Elijah@users.noreply.github.com>
2018-08-20 21:14:47 +0100

    added space btwin cart and number

--- app.js
+++ app.js
@@ -2,24 +2,29 @@ window.onload = function () {
     console.clear();
     var cart = document.querySelector("#cart");
     const buy = document.querySelectorAll("#buy");
-cart.textContent = 0;
+    var num = cart.textContent;
+    
     buy.forEach( (card) => {
         card.addEventListener("click", addToCart);
 
-    function addToCart() {
+    function addToCart(e) {
 
         
         // cart.textContent = Number(1);
         // cart.textContent = add();
         
-
-        if( cart.textContent ){
-            cart.textContent = (Number(cart.textContent)+1);
-            console.log(cart.textContent +1);
+        
+        if( cart.textContent){
+            if( e.target.textContent){
+                num = (Number(cart.textContent) + 1);
+                cart.textContent = ' '+num;
+            console.log(e.target.firstElementChild);
+            console.log
+        }
         }
         else{ 
             console.log(typeof cart)
-            cart.textContent = 1;
+            cart = 1;
         }
         
         }
