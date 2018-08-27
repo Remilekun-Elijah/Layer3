0f7e9b0a609de2f60dc9a3075334b94aae0af4b4
Remilekun-Elijah <38536025+Remilekun-Elijah@users.noreply.github.com>
2018-08-27 12:22:26 +0100

    more details on the cards

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var console, window;
 window.onload = function () {
-    "use-strict";
+//    "use strict";
+
+
     console.clear();
 
     // var html = document.querySelector('html'), body = document.querySelector("body");
 
     let cart = document.querySelector("#cart");
+    cart.style.cursor = "pointer";
     const buy = document.querySelectorAll("#buy");
     let cartItem = document.createElement('div');
         
@@ -15,30 +18,40 @@ window.onload = function () {
         let containerDiv = this.document.createElement('div');
         containerDiv.classList = 'pt-1 pr-3 pb-1 pl-0';
     
+        
     buy.forEach( card => {
         
+        
+
     var addToCart = e => {
         if(cart.textContent){
             if( e.target.textContent){
-                num = (Number(span.textContent) + 1);
-                
-                span.textContent = ' '+num;
+                num = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.firstElementChild.firstElementChild.firstElementChild.childElementCount+1;
+
+                var order = e.target.parentElement.parentElement.children[2].firstElementChild.children[2].firstElementChild.firstElementChild;
+
+                 order.textContent = (Number(order.textContent) + 1);
+               
+               
+                span.textContent = ' '.concat(num);
                 span.style.color = 'limegreen';
-                cart.style.cursor = "pointer";
+                
                 cart.appendChild(span);
 
                 var targets = e.target.parentElement.parentElement.firstElementChild.firstElementChild;
                 // console.log(targets);
-                let number = document.getElementById('number');
-                console.log(e.target.parentElement.parentElement.nextElementSibling.children[2].firstElementChild);
-                console.log(number)
+
                 // create & add element to cart for each clicked item
                 var div = document.createElement('div'), img = document.createElement('img'), del = document.createElement('span');
 
-                let details, money = e.target.parentElement.parentElement.nextElementSibling.children[2].firstElementChild.innerText, ul = document.createElement('ul');
+                let details, ul = document.createElement('ul'); 
+        var price = e.target.parentElement.parentElement.children[2].firstElementChild.firstElementChild.firstElementChild.textContent;
+        // console.log(price);
+        
+
                 details = `
                     <li class='mr-5'><i class='fa fa-map-marker'></i> Law School</li>
-                    <li><i class='fa fa-money text-danger'> ${money} </i></li>
+                    <li><i class='fa fa-money text-danger'> ${price} </i></li>
                 `;
                 ul.innerHTML = details;
                 ul.classList = ' d-flex  flex-row mt-4 text-secondary';
@@ -59,17 +72,30 @@ window.onload = function () {
                 div_style.marginBottom = '5px';
                 div.insertBefore(ul, del);
                 
-                // cartItem.style.display = 'none';
+                // cartIte m.style.display = 'none';
                 cartItem.classList.replace("show","hide");
 
-                containerDiv.append(div);            
+                containerDiv.append(div);
+                del.addEventListener('click', e => {
+                    
+                    if(e.target.className.includes('fa')){
+                        e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+                        var up;
+                        up = (Number(span.textContent) -1);
+                        console.log(up);
+                        span.textContent = ' '.concat(up);
+                        order.innerText = (Number(order.innerText) -1);
+                        // console.log(e.target.parentElement);
+                    }
+                });
+
             }
             else{
                 alert('error');
             }
         }
         else{ 
-            console.log(typeof cart.textContent);
+            // console.log(typeof cart.textContent);
             cart.textContent = 1;
         }
         
@@ -103,16 +129,52 @@ window.onload = function () {
                     
                     page2.classList = ' clap-in';
                     
-                    
-                    console.log(cartItem.childElementCount);
-                
-                    
                 }else if(page1.className.includes('show')){
                     page1.classList.replace("show", "hide");
                     page2.classList = ' clap-out';
                 
                 }
                 
+                if(window.innerWidth <= 550 ){
+                    document.querySelector("footer").classList = ' foot-cart';
+                    document.querySelector("footer").classList.toggle('foot-cart');
+                    console.log(document.querySelector("footer"));
+                }
+            
             };
         cart.addEventListener('click', showCart);
+
+
+
+/////////////////////////
+var arr = [];
+        var order = document.querySelectorAll('#order');
+        amount = order.textContent;
+        
+        // console.log(price);
+        
+        
+// console.log(arr);
+//order.forEach((orders, e) => {
+//    // var price = e.target.parentElement.parentElement.children[2].firstElementChild.firstElementChild.firstElementChild.textContent;
+//    buy.forEach(buys => { 
+//    buys.addEventListener('click', function(){
+//        if(arr.length !== 0){
+//         
+//          
+//            orders.textContent = arr.push(Number(arr.length+1));
+//            
+//            // console.log('from if ' + arr);
+//            // arr = [price];
+//        }else{
+//          
+//          
+//            
+//            orders.textContent = arr.push(arr.length+1);
+//            // console.log('from else '+ arr);
+//        }
+//    })
+//    });
+//});
+
 };
