fc4944a2a92156c405458ed2a9573bccef2eb80f
remilekun-elijah <remilekunelijah21997@gmail.com>
2021-03-07 19:35:41 +0100

    initial commit

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get('/location/moshood', (req, res) => {
-    res.sendFile(__dirname + '/public/location/moshood.html')
+    res.sendFile(__dirname + '/public/location/moshood.html');
 });
 app.get('/location/law-school', (req, res) => {
-    res.sendFile(__dirname + '/public/location/law-school.html')
+    res.sendFile(__dirname + '/public/location/law-school.html');
 });
 app.get('/location/landmark', (req, res) => {
-    res.sendFile(__dirname + '/public/location/landmark.html')
+    res.sendFile(__dirname + '/public/location/landmark.html');
 });
 app.get('/location/jide-oki', (req, res) => {
-    res.sendFile(__dirname + '/public/location/jide-oki.html')
+    res.sendFile(__dirname + '/public/location/jide-oki.html');
 });
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("App listening on port " + port))
+app.listen(port, () => console.log("App listening on port " + port));
--- public/assets/templates/navbar.js
+++ public/assets/templates/navbar.js
@@ -1,18 +1,9 @@
-const navs = document.createElement("nav");
-navs.id = 'navigation';
-
-function nav(home, location) {
-    if (document.location.href.includes('location')) {
-        home = '../index.html';
-        location = ''
-
-    } else {
-        location = 'location/';
-        home = 'index'
-    };
-    navs.innerHTML = `<nav class="navbar navbar-expand-sm bg-danger shadow-lg stickey-top">
-                <div class="container">
-                    <a href="${home}" class=" navbar-brand" style='text-shadow: 2px 2px 2px #000'> <i class="fa fa-cutlery text-dark" style='font-size: 30px;'></i>
+//jshint esversion:6
+const nav = document.createElement("nav");
+nav.id = 'navigation';
+nav.className = "navbar navbar-expand-sm bg-danger shadow-lg stickey-top";
+nav.innerHTML = `<div class="container">
+                    <a href="/" class=" navbar-brand" style='text-shadow: 2px 2px 2px #000'> <i class="fa fa-cutlery text-dark" style='font-size: 30px;'></i>
                         <b class="text-white">Layer3</b><b class="text-white">-</b><b class="text-white">Food</b></a>
                     <button class="navbar-toggler py-2 border border-dark" style="border-width: 2px; border-style: double; background: #fff; color: #dc3545;"
                      data-toggle="collapse" data-target="#navbar">
@@ -21,19 +12,19 @@ function nav(home, location) {
                     <div class="collapse navbar-collapse" id="navbar">
                         <ul class="nav navbar-nav ml-auto">
                             <li class="nav-item">
-                                <a href="${location}moshood" class="nav-link">Moshood</a>
+                                <a href="/location/moshood" class="nav-link">Moshood</a>
                             </li>
                             <li class="nav-item">
-                                <a href="${location}landmark" class="nav-link">Landmark</a>
+                                <a href="/location/landmark" class="nav-link">Landmark</a>
                             </li>
                             <li class="nav-item">
-                                <a href="${location}jide-oki" class="nav-link">Jide Oki</a>
+                                <a href="/location/jide-oki" class="nav-link">Jide Oki</a>
                             </li>
     
                             <li class="nav-item dropdown ml-md-5 ">
                                 <a href="#" class="nav-link dropdown-toggle" id="user" data-toggle="dropdown">Guest</a>
                                 <div class="dropdown-menu">
-                                    <a href="${home}" class="dropdown-item text-danger" id="login"><i class="fa fa-user-plus"></i> Register</a>
+                                    <a href="/" class="dropdown-item text-danger" id="login"><i class="fa fa-user-plus"></i> Register</a>
                                     <a href="#" class="dropdown-item d-none text-danger" id="settings" data-target="#updateModal" data-toggle="modal"><i class="fa fa-cog"></i> Settings</a>
                                     <a href="#" class="dropdown-item d-none text-danger" id="unregister" data-target="#logoutModal" data-toggle="modal">
                                         <i class="fa fa-user-times"></i> Unregister</a>
@@ -52,10 +43,6 @@ function nav(home, location) {
                             -->
                         </ul>
                     </div>
-                </div>
-            </nav>`;
+                </div>`;
 
-    document.querySelector('body').insertBefore(navs, document.getElementById('logoutModal'));
-
-}
-nav();
+document.querySelector('body').insertBefore(nav, document.getElementById('logoutModal'));
